d9c850d4dce3d43447371d9416ea446f5f4e7a83
Cory Olson and Maldon Meehan <maldon@>
2016-06-29 15:04:40 -0700

    add navebar active class and js file

--- app.rb
+++ app.rb
@@ -7,51 +7,51 @@ require('./lib/palindrome')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @page_title="home"
   erb(:index)
 end
 
 get('/palindrome') do
+  @page_title="palindrome"
   erb(:palindrome)
 end
 
 post('/palindrome') do
+  @page_title="palindrome"
   word = params.fetch('word')
   @result = (params.fetch('word').palindrome?()) ? "#{word} is a palindrome" : "#{word} is not a palindrome"
   erb(:palindrome)
 end
 
-
-get('/palindrome_results') do
-  word = params.fetch('word')
-  @result = (params.fetch('word').palindrome?()) ? word + " is a palindrome" : word + " is not a palindrome"
-  erb(:palindrome_results)
-end
-
-
 get('/titlecase') do
+  @page_title="titlecase"
   erb(:titlecase)
 end
 
 get('/title') do
+  @page_title="titlecase"
   @title = params.fetch('title').title_case()
   erb(:title)
 end
 
 get('/scrabble') do
+  @page_title="scrabble"
   erb(:scrabble)
 end
 
 get('/scrabblescore') do
+  @page_title="scrabble"
   @score = params.fetch('word').scrabble()
   erb(:scrabblescore)
 end
 
 get('/rock_paper_scissors') do
+  @page_title="rps"
   erb(:rock_paper_scissors)
 end
 
 get('/rps_winner') do
-
+  @page_title="rps"
   @player1 = params.fetch('player1')
   @player1_name = "Player 1"
   @player2 = params.fetch('player2')
